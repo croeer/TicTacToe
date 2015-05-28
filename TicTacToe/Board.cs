9d8e8d394111c18b6e3a7d8fe560217af59cfde2
Christian Röer <croeer@MacBook-Pro.localdomain>
2015-05-28 07:33:03 +0200

    Initial commit TicTacToe
    Committer: croeer <christian@roeer.info>

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using CocosSharp;
+
+namespace TicTacToe
+{
+	public class Board : CCLayerColor
+	{
+		public Board() : base()
+		{
+		}
+
+	}
+
+}
